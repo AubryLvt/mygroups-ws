f3cf6477a5987c3aa4c398345cb23220fd34f0e2
Aubry Thomas LOUVET <129608889+AubryLvt@users.noreply.github.com>
2024-04-06 15:49:32 +0200

    Initial commit

---
+++ src/test/java/app/mygroupsws/MygroupsWsLayerApplicationTests.java
@@ -0,0 +1,13 @@
+package app.mygroupsws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MygroupsWsLayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
